76d5502d0e38f8b18db97d229154e355f6cea1f8
Chris Mellor <chrismellor@outlook.com>
2017-06-08 10:45:01 -0500

    Ugh. Fixed the things I didn't do

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-
 function makePyramid(){
+	
 	var symbol = document.getElementById("whichChar").value;
 	var rows = document.getElementById("numOfRows").value;
-	document.write("<center>");
-	for(i=0; i<=rows; i++) {
-		for(var x=0; x<=i; x=x + .5) {
-			document.write(symbol);
+	
+	for(i=0; i<rows; i++) {
+		var consoleOutput = "";
+
+		for(var x=0; x<rows-i; x++) {
+			consoleOutput += " ";
 		}
-		document.write("<br>");
+
+		for (var z= 0; z<=i; z=z+.5){
+			consoleOutput += symbol;
+		}
+		console.log(consoleOutput);
 	}
 }
 
 
+document.getElementById('numOfRows').addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+            makePyramid();
+        }
+    });
 
+document.getElementById('whichChar').addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+            makePyramid();
+        }
+    });
